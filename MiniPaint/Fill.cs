5c14ef8693722d5f2e31f44bbd402cce76bc325b
Nastia <cepebpa@yandex.ru>
2018-11-12 18:32:56 +0300

    Херня

--- MiniPaint/Fill.cs
+++ MiniPaint/Fill.cs
@@ -57,25 +57,31 @@ namespace MiniPaint
             return;
         }
 
-
-
-
-
-
-
-
-
-
-
         private void Recursion(int x, int y, Color begin, Color newCol)
         {
-            if (x >= 0 && y >= 0 && y <= pbx.Height && x <= pbx.Width && bmp.GetPixel(x, y)==begin&& bmp.GetPixel(x, y) !=newCol)           
+            List <Point> listik = new List<Point>();
+            List<Point> listik_2 = new List<Point>();
+            listik.Add(new Point(x, y));
+
+            while (listik.Count > 0)
             {
-                    bmp.SetPixel(x, y, newCol);
-                    Recursion(x - 1, y, begin, newCol);
-                   // Recursion(x + 1, y, begin, newCol);
-                   // Recursion(x, y - 1, begin, newCol);
-                   Recursion(x, y + 1, begin, newCol);
+                x = listik[0].X;
+                y = listik[0].Y;
+                if (x > 0 && y > 0 && x< pbx.Height && y< pbx.Width && bmp.GetPixel(x, y) == begin && bmp.GetPixel(x, y) != newCol)
+                {
+                    bmp.SetPixel(x, y, newCol);                    
+                    if(!listik.Contains(new Point(x - 1, y))&& !listik_2.Contains(new Point(x - 1, y)))
+                    listik.Add(new Point(x - 1, y));
+                    if (!listik.Contains(new Point(x + 1, y)) && !listik_2.Contains(new Point(x + 1, y)))
+                        listik.Add(new Point(x+1, y));
+                    if (!listik.Contains(new Point(x, y-1)) && !listik_2.Contains(new Point(x, y-1)))
+                        listik.Add(new Point(x, y-1));
+                    if (!listik.Contains(new Point(x, y+1)) && !listik_2.Contains(new Point(x, y+1)))
+                        listik.Add(new Point(x, y+1));
+                }
+                listik_2.Add(new Point(x, y));
+                listik.RemoveAt(0);
+                //listik.Remove(new Point (x,y));
             }
         }
     }
