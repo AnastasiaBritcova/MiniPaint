67dc0102d4479b0ebe1792ca2f1c88949ed9da7f
Nastia <cepebpa@yandex.ru>
2018-11-15 00:15:25 +0300

    Уменьшалка работай!

--- MiniPaint/Form1.cs
+++ MiniPaint/Form1.cs
@@ -317,7 +317,7 @@ namespace MiniPaint
         }
         private void RubberMinusBTN_Click(object sender, EventArgs e)
         {
-            if (buffer.Pen.Width > 5)
+            if (buffer.BackPen.Width > 5)
             {
                 buffer.ChangeBackPenWigth(buffer.BackPen.Width - 3);
                 DrawRubber();
